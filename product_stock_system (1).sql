6d0fc3e4aa96bce7a30daab161670604e87fd9fe
dedenha <149679667+dedenha@users.noreply.github.com>
2024-09-21 17:37:31 +0700

    Day-13-SQL-Triggers-Store-Procedure

---
+++ product_stock_system (1).sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Sep 2024 pada 12.32
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product_stock_system`
+--
+
+DELIMITER $$
+--
+-- Prosedur
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getSiswaByBorn` (IN `tempatLahir` VARCHAR(50))   BEGIN
+  SELECT * FROM siswa
+  WHERE TTL LIKE CONCAT('%', tempatLahir, '%');
+END$$
+
+--
+-- Fungsi
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `getJmlByGender` (`inputGender` CHAR(1)) RETURNS INT(11) DETERMINISTIC BEGIN
+  DECLARE jumlah INT;
+  SELECT COUNT(*) INTO jumlah
+  FROM siswa
+  WHERE gender = inputGender;
+  RETURN jumlah;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id` int(11) NOT NULL,
+  `nilai_IPA` decimal(5,2) DEFAULT NULL,
+  `nilai_IPS` decimal(5,2) DEFAULT NULL,
+  `nilai_MTK` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `harga` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `nama`, `harga`) VALUES
+(1, 'Produk A', 50000.00);
+
+--
+-- Trigger `products`
+--
+DELIMITER $$
+CREATE TRIGGER `insertProductToStock` AFTER INSERT ON `products` FOR EACH ROW BEGIN
+  INSERT INTO stock (id, quantity) VALUES (NEW.id, 0);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL,
+  `NIS` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `TTL` varchar(50) NOT NULL,
+  `gender` enum('L','P') NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `stock`
+--
+
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `stock`
+--
+
+INSERT INTO `stock` (`id`, `quantity`) VALUES
+(1, 0);
+
+--
+-- Trigger `stock`
+--
+DELIMITER $$
+CREATE TRIGGER `checkStock` AFTER UPDATE ON `stock` FOR EACH ROW BEGIN
+  IF NEW.quantity < 10 THEN
+    SIGNAL SQLSTATE '45000' 
+    SET MESSAGE_TEXT = 'Peringatan: Stok produk kurang dari 10!';
+  END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
